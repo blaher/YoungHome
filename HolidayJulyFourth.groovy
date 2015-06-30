9264faf38a0e19a2a721651b67d2b9782ac2040a
Benjamin J. Young <blaher09@sbcglobal.net>
2015-06-30 10:35:03 -0400

    Create HolidayJulyFourth.groovy

---
+++ HolidayJulyFourth.groovy
@@ -0,0 +1,7 @@
+definition(
+	name: "Holiday July Fourth",
+	namespace: "younghome",
+	author: "Benjamin J. Young",
+	description: "Flashes the entire home's lights red/white/blue, and blasts Beethoven's Symphony No. 9 over the sonos speakers throughout the house.",
+	category: "Convenience"
+);
