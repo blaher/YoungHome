b2d42819aaf2fc6fff1cd3435f42da90720f0249
Benjamin J. Young <blaher09@sbcglobal.net>
2015-06-30 10:36:53 -0400

    Create HolidayChristmas.groovy

---
+++ HolidayChristmas.groovy
@@ -0,0 +1,7 @@
+definition(
+	name: "Holiday Christmas",
+	namespace: "younghome",
+	author: "Benjamin J. Young",
+	description: "Flashes the entire house's colors, and plays random christmas music over sonos while away and movement is detected in front.",
+	category: "Convenience"
+);
