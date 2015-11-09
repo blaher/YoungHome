490f1c9a1655913e3b0a0ae76e5b618dee74c886
Benjamin J. Young <blaher09@sbcglobal.net>
2015-11-09 10:49:35 -0500

    Update BedroomAlarm.groovy

--- Apps/BedroomAlarm.groovy
+++ Apps/BedroomAlarm.groovy
@@ -15,32 +15,76 @@ preferences {
     section('Select Asleep Mode') {
 		input('asleep_mode', 'mode');
 	}
+    section('Devices') {
+    	input('lights', 'capability.switchLevel', title:'Lights', multiple:true);
+    	input('motion', 'capability.motionSensor', title:'Motion Sensor');
+    }
 }
 
 def installed() {
 	log.debug("Installed with settings: ${settings}");
-	scheduleaTimes();
+    states();
+	schedules();
 }
 
 def updated(settings) {
 	log.debug('Application updated');
+    unsubscribe();
 	unschedule();
-	scheduleTimes();
+    states();
+	schedules();
 }
 
-def scheduleTimes() {
+def states() {
+	state.lightLevel = 100;
+}
+
+def schedules() {
 	log.debug('Scheduling times');
 
-	schedule("0 0 6 ? * 2-6", wakeUp); // Mon - Fri
-    schedule("0 30 21 ? * 1-5", gotoSleep); // Sun - Thur
+	schedule("0 45 5 ? * 2-6", wakeUp); // Mon - Fri
+    schedule("0 0 10 ? * 2-6", checkMotion); // Mon - Fri
 }
 
 def wakeUp() {
-	log.debug('Good Morning.');
-	setLocationMode(awake_mode);
+	log.debug('You\'re about to get up');
+    subscribe(motion, 'motion', checkMotion);
 }
 
-def gotoSleep() {
-	log.debug('Good Night.');
-	setLocationMode(asleep_mode);
+def checkMotion(evt) {
+	log.debug('Good morning');
+    
+    unsubscribe(motion);
+	
+    lights.setLevel(100);
+    setLocationMode(awake_mode);
+    
+    runIn(60*1, startLevelUp);
+}
+
+def startLevelUp() {
+	log.debug('Starting the level up process');
+
+	lights.setLevel(10);
+	runIn(60*1, levelUp);
+}
+
+def levelUp() {
+	log.debug('Leveling up');
+    
+    state.lightLevel = state.lightLevel + 3;
+    lightLevel(state.lightLevel);
+    
+    unschedule(levelUp);
+    
+    if (state.lightLevel < 99) {
+    	runIn(60*1, levelUp);
+    }
+}
+
+def lightLevel(value) {
+	log.debug('Changing light level');
+
+	state.lightLevel = value;
+	lights.setLevel(value);
 }
